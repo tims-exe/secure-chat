dced27e86f1155246e6cc9991a338715bf55b0fd
tims-exe <timonkjohn@gmail.com>
2025-12-08 22:03:25 +0530

    added redis client and store room metadata with expiry

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,8 +1,21 @@
+import { redis } from '@/lib/redis'
 import { Elysia } from 'elysia'
+import { nanoid } from 'nanoid'
+
+const ROOM_TTL_SECONDS = 60 * 10
 
 const rooms = new Elysia({ prefix: "/room"})
-    .post("/create", () => {
-        console.log("CREATE A NEW ROOM`")
+    .post("/create", async () => {
+        const roomId = nanoid()
+
+        await redis.hset(`meta:${roomId}`, {
+            connected: [],
+            createdAt: Date.now()
+        })
+
+        await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS)
+
+        return { roomId }
     })
 
 const app = new Elysia({ prefix: '/api' })
