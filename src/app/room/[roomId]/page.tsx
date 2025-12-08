413e451280b7d0a8ee1f45aaa6dd82bc2d92b167
tims-exe <timonkjohn@gmail.com>
2025-12-08 22:55:09 +0530

    styling chat layout

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useParams } from "next/navigation"
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 
 function formatTimeRemaining(seconds: number) {
@@ -18,6 +18,8 @@ export default function RoomPage () {
 
     const [copyStatus, setCopyStatus] = useState("COPY")
     const [timeRemaining, setTimeRemaining] = useState<number | null>(null)
+    const [input, setInput] = useState("")
+    const inputRef = useRef<HTMLInputElement>(null)
 
     function copyLink () {
         const url = window.location.href
@@ -64,7 +66,37 @@ export default function RoomPage () {
                 </button>
             </header>
 
-            
+            <div className="flex-1 overflow-y-auto p-4 space-y-4 scrollbar-thin">
+
+            </div>
+
+            <div className="p-4 border-t border-zinc-800 bg-zinv-900/30">
+                <div className="flex gap-4">
+                    <div className="flex-1 relative group">
+                        <span className="absolute left-4 top-1/2 -translate-y-1/2 text-green-500 animate-pulse">
+                            {">"}
+                        </span>
+                        <input
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter" && input.trim()) {
+                                // TODO: SEND MSG
+                                inputRef.current?.focus()
+                            }
+                        }}
+                        autoFocus 
+                        type="text" 
+                        placeholder="Type message..."
+                        className="w-full bg-black border border-zinc-800 focus:border-zinc-700 focus:outline-none transition-colors text-zinc-100 placeholder:text-zinc-700 py-3 pl-8 pr-4 text-sm" />
+                    </div>
+
+                    <button 
+                    className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer">
+                        SEND
+                    </button>
+                </div>
+            </div>
         </main>
     )
 }
